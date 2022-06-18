efeb4b507dcc9716ac50289b6f6540c1e1e9344d
Marilynn Stone <hello@marilynnstone.com>
2022-06-18 23:56:18 +0000

    create vertical search and return if false

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const horizontalWordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  const newArry = [];
+  for (let row = 0; row < matrix[0].length; row++) {
+    const colArry = [];
+    for (let col = 0; col < matrix.length; col++) {
+      colArry.push(matrix[col][row]);
+    }
+    newArry.push(colArry);
+  }
+  return newArry;
+};
+
+const wordSearch = (letters, word) => {
+  if (horizontalWordSearch(letters, word)) {
+    return true;
+  }
+  const tranposedLetters = transpose(letters);
+  if (horizontalWordSearch(tranposedLetters, word)) {
+    return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
