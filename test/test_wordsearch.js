93ffe74fc9b6c88c4ccd83af4f3c7fece5bba728
Marilynn Stone <hello@marilynnstone.com>
2022-06-19 21:50:00 +0000

    Co-authored-by: Robert Servado @ArjayS

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -38,16 +38,48 @@ describe("#wordSearch()", function() {
   
   it("should return true if the word is present vertically", function() {
     const result = wordSearch([
-      ['l', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['i', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['g', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['h', 'l', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['t', 'i', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['n', 'g', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['i', 'h', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['n', 't', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['g', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'lightning');
+      ['L', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['G', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'l', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['T', 'i', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['N', 'g', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['I', 'h', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['N', 't', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['G', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LIGHTNING');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present horizontally in reverse", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'A'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['L', 'I', 'O', 'F', 'O', 'R', 'E', 'A'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'AEROFOIL');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically in reverse", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'A'],
+      ['S', 'E', 'L', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'I', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'O', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'F', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'O', 'E', 'N', 'E', 'Y', 'B'],
+      ['L', 'I', 'R', 'F', 'O', 'R', 'E', 'A'],
+      ['O', 'D', 'E', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'A', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'AEROFOIL');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,13 +1,3 @@
-const horizontalWordSearch = (letters, word) => {
-  const horizontalJoin = letters.map(ls => ls.join(''));
-  for (let l of horizontalJoin) {
-    if (l.includes(word)) {
-      return true;
-    }
-  }
-  return false;
-};
-
 const transpose = function(matrix) {
   const newArry = [];
   for (let row = 0; row < matrix[0].length; row++) {
@@ -20,12 +10,38 @@ const transpose = function(matrix) {
   return newArry;
 };
 
+const horizontalWordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const reverseSearch = (letters, word) => {
+  const reversed = letters.map((as) => as.reverse().join(""));
+  for (let a of reversed) {
+    if (a.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
 const wordSearch = (letters, word) => {
   if (horizontalWordSearch(letters, word)) {
     return true;
   }
   const tranposedLetters = transpose(letters);
   if (horizontalWordSearch(tranposedLetters, word)) {
+    return true;
+  }
+  if (reverseSearch(letters, word)) {
+    return true;
+  }
+  if (reverseSearch(tranposedLetters, word)) {
     return true;
   }
   return false;
